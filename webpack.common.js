e66b040a371e8e051cd5662d2ef70a2f57c60bcf
PearlLiu <hc.harrycheng@gmail.com>
2017-11-29 22:49:39 +0800

    add hash

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
     app: './src/index.js',
-    print: './src/print.js'
+    print: './src/print.js',
+    another:'./src/another-module.js'
   },
 
   plugins:[
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title:'out put managment'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+        name:'common'
     })
   ],
 
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[chunkhash].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
 
